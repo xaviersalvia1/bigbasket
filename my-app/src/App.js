e2aecb84bc379b6194c95f526769783f62b0d82e
Gautam Kumar <er.gkp7764@gmail.com>
2022-11-11 13:00:58 +0530

    day4

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from "./Components/Home"
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hwllo world</h1>
+     <Home/>
+     <Navbar/>
+    </div>
+  );
+}
+
+export default App;
