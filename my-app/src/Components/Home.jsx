78091a0be8a1fbf36db5747fd8a719d49bb6020f
Gautam Kumar <er.gkp7764@gmail.com>
2022-11-12 20:57:47 +0530

    day5

--- my-app/src/Components/Home.jsx
+++ my-app/src/Components/Home.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Home = () => {
+  const [data,setData]=useState([]);
+  const GetData=()=>{
+    fetch('https://fakestoreapi.com/products/1')
+            .then(res=>res.json())
+            .then(json=>
+            setData(json)
+            )
+  }
+  
   return (
+    <>
     <div>
-      
+      {GetData()}
+      {/* {data.map((el)=>{
+        console.log(el)
+      })} */}
     </div>
+    </>
   )
 }
 
